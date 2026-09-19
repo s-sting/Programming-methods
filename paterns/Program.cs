0965b9bac16cad5f5d2b97608ed98d13b35d4281
Полуянов Андрей Сергеевич <a.poluyanov@univ.priv>
2026-09-19 09:34:42 +0300

    init

---
+++ paterns/Program.cs
@@ -0,0 +1,22 @@
+﻿using paterns.generetion;
+using paterns.generetion.AbstractFactory;
+using paterns.generetion.FactoryMethod;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paterns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Singlton.Run();
+            //Prototype.Run();
+            //FactoryMethod.Run();
+            AbstractFactory.Run();
+        }
+    }
+}
